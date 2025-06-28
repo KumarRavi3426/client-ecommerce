b84426ee0201b187197063d39dcd0d2f68913785
Kumar Ravi <kumarravi16082@gmail.com>
2025-06-28 15:10:29 +0530

    updated a string to literal in axios request in cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -63,7 +63,7 @@ const CartPage = () => {
     try {
       setLoading(true);
       const { nonce } = await instance.requestPaymentMethod();
-      const { data } = await axios.post("${process.env.REACT_APP_API_URL}/api/v1/product/braintree/payment", {
+      const { data } = await axios.post(`${process.env.REACT_APP_API_URL}/api/v1/product/braintree/payment`, {
         nonce,
         cart,
       });
